c83a891c3c96c4d973727f83424cdab35b947742
n00bS-oWn-m3 <sebastiaan@onyx.be>
2023-11-13 17:30:29 +0100

    started from scratch

--- Wedding.sol
+++ Wedding.sol
@@ -1,87 +1,64 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.21;
 
-import "./WeddingCertificate.sol"; // Assuming WeddingCertificate contract is in the same directory
-
-contract Wedding {
-
-    struct Couple {
-        address fiance1;
-        address fiance2;
+contract WeddingContract {
+    struct Engagement {
+        address parner1;
+        address partner2;
         uint256 weddingDate;
         bool isEngaged;
         bool isMarried;
-        address[] guestList;
-        uint256 negativeVotes;
     }
 
-    mapping(address => Couple) public engagements;
-    mapping(address => bool) public authorisedAccounts;
-    address public weddingCertificateAddress;
+    mapping(address => Engagement) public engagements;
 
-    modifier notEngaged(address fiance) {
-        require(engagements[fiance].fiance1 == address(0) && engagements[fiance].fiance2 == address(0), "Fiance is already engaged");
-        //check if previus line is correct also if fiance is not in the engagement dictionary
+    // Modifiers
+    // Check if user is not yet engaged
+    modifier notAlreadyEngaged(address user) {
+        require(!engagements[user].isEngaged, "User is already engaged");
         _;
     }
 
-    constructor(address _weddingCertificateAddress) {
-        weddingCertificateAddress = _weddingCertificateAddress;
+    // Check if a user is already engaged
+    modifier isEngaged(address user) {
+        require(engagements[user].isEngaged, "User is not yet engaged");
+        _;
     }
 
-    function engage(address _fiance, uint256 _weddingDate) external notEngaged(msg.sender) notEngaged(_fiance) {
-        //check if is still works if one send before the other because maybe the other result engaged
-        //also add for the other fiance
-        engagements[msg.sender] = Couple({
-            fiance1: msg.sender,
-            fiance2: _fiance,
-            weddingDate: _weddingDate,
-            isEngaged: true,
-            isMarried: false,
-            guestList: new address[](0),
-            negativeVotes: 0
-        });
+    // Check if user is not yet married
+    modifier notAlreadyMarried(address user) {
+        require(!engagements[user].isMarried, "User is already married");
+        _;
     }
 
-    function confirmGuestList(address[] calldata _guestList) external {
-        require(engagements[msg.sender].isEngaged, "Not engaged");
-        engagements[msg.sender].guestList = _guestList;
+    // Check if a user is already engaged
+    modifier isMarried(address user) {
+        require(engagements[user].isMarried, "User is not yet married");
+        _;
     }
 
-    function revokeEngagement() external {
-        require(engagements[msg.sender].isEngaged, "Not engaged");
-        delete engagements[msg.sender];
+    // Check if wedding date is in the future
+    modifier validWeddingDate(uint256 weddingDate) {
+        require(weddingDate > block.timestamp, "Wedding date must be in the future");
+        _;
     }
 
-    function marry() external {
-        require(engagements[msg.sender].isEngaged, "Not engaged");
-        require(block.timestamp <= engagements[msg.sender].weddingDate, "Not the wedding day");
-        engagements[msg.sender].isMarried = true;
-        
-        // Issue wedding certificate NFT
-        WeddingCertificate(weddingCertificateAddress).mint(engagements[msg.sender].fiance1, engagements[msg.sender].fiance2);
+    // Engage two users
+    function engage(address p1, address p2, uint256 weddingDate) 
+        public 
+        notAlreadyEngaged(p1) 
+        notAlreadyEngaged(p2) 
+        notAlreadyMarried(p1) 
+        notAlreadyMarried(p2) 
+        validWeddingDate(weddingDate) 
+    {
+        engagements[p1] = Engagement(p1, p2, weddingDate, true, false);
+        engagements[p2] = Engagement(p1, p2, weddingDate, true, false);
     }
 
-    function voteAgainstWedding() external {
-        require(engagements[msg.sender].isEngaged, "Not engaged");
-        for(uint256 i = 0; i < engagements[msg.sender].guestList.length; i++) {
-            if(engagements[msg.sender].guestList[i] == msg.sender) {
-                engagements[msg.sender].negativeVotes++;
-                break;
-            }
-        }
-
-        if(engagements[msg.sender].negativeVotes > engagements[msg.sender].guestList.length / 2) {
-            engagements[msg.sender].isEngaged = false;
-        }
-    }
-
-    function addAuthorisedAccount(address _account) external {
-        authorisedAccounts[_account] = true;
-    }
-
-    function removeAuthorisedAccount(address _account) external {
-        authorisedAccounts[_account] = false;
+    function getEngagementDetails(address user) 
+        public view isEngaged(user) returns (Engagement memory)
+    {
+        return engagements[user];
     }
 }
-
